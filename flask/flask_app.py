e08cfa4819d52ca85f8ad4c0a5cc47ba6f04e37a
Kirubel K <joejnke@gmail.com>
2019-11-05 16:27:46 +0300

    using variable rules

--- flask/flask_app.py
+++ flask/flask_app.py
@@ -3,10 +3,20 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return "Hello World"
+@app.route('/<name>')
+def hello_world(name):
+    return "Hello - %s" % name
+
+
+@app.route('/blog/<int:postID>')
+def show_blog(postID):
+    return 'Blog Number %d' % postID
+
+
+@app.route('/rev/<float:revNo>')
+def revision(revNo):
+    return 'Revision Number %f' % revNo
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
