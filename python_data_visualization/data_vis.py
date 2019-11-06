fd0850fa506b072b3c4bbf58ad190712ef719058
Kirubel K <joejnke@gmail.com>
2019-11-06 17:34:37 +0300

    scatter and line ploting with seaborn

--- python_data_visualization/data_vis.py
+++ python_data_visualization/data_vis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 iris = pd.read_csv('iris.csv', names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
@@ -7,6 +8,9 @@ iris = pd.read_csv('iris.csv', names=['sepal_length', 'sepal_width', 'petal_leng
 
 # iris.plot.scatter(x='sepal_length', y='sepal_width', title='Iris Dataset')
 # iris.plot.hist(subplots=True, layout=(4,1), figsize=(10, 10), bins=20)
-iris.drop(['class'], axis=1).plot.line(subplots=True, layout=(4,1), figsize=(10, 10))
-plt.show()
+# iris.drop(['class'], axis=1).plot.line(subplots=True, layout=(4, 1), figsize=(10, 10))
+# plt.show()
 
+sns.scatterplot(x='sepal_length', y='sepal_width', data=iris, hue='class')
+sns.lineplot(data=iris.drop(['class'], axis=1))
+plt.show()
