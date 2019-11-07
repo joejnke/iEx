34929517546cb1c676eb744bcc462f91c76e1b55
Kirubel K <joejnke@gmail.com>
2019-11-08 00:08:06 +0300

    initial functional implementation

--- python_data_visualization/data_vis.py
+++ python_data_visualization/data_vis.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 iris = pd.read_csv('iris.csv', names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
 # print(iris.head())
+nunet_stat = pd.read_csv('sample_cpu_mem_usage.csv', names=['mem_usage', 'cpu_usage'])
 
 
 # iris.plot.scatter(x='sepal_length', y='sepal_width', title='Iris Dataset')
@@ -11,6 +12,8 @@ iris = pd.read_csv('iris.csv', names=['sepal_length', 'sepal_width', 'petal_leng
 # iris.drop(['class'], axis=1).plot.line(subplots=True, layout=(4, 1), figsize=(10, 10))
 # plt.show()
 
-sns.scatterplot(x='sepal_length', y='sepal_width', data=iris, hue='class')
-sns.lineplot(data=iris.drop(['class'], axis=1))
+# sns.scatterplot(x='sepal_length', y='sepal_width', data=iris, hue='class')
+# sns.lineplot(data=iris.drop(['class'], axis=1))
+# sns.lineplot(data=nunet_stat.drop('mem_usage', axis=1))
+nunet_stat.plot.line(subplots=True, layout=(2, 1), figsize=(10, 10))
 plt.show()
---
+++ python_data_visualization/nunet_stat_vis.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def stat_vis():
+    names = ['mem_usage', 'cpu_usage', 'hdd_usage', 'network_usage']  # first row of the csv file, if available
+    nunet_stat = pd.read_csv('sample_cpu_mem_usage.csv', names=names)
+    nunet_stat.plot.line(subplots=True, layout=(4, 1), figsize=(10, 10))
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+    stat_vis()
